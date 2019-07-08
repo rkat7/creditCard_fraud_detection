548253802a2a0a85329f757a9ed16f2ca5a9ec87
Rohith Kattamuri <50071276+rkat7@users.noreply.github.com>
2019-07-08 20:50:34 +0530

    Deployed the model

--- model.py
+++ model.py
@@ -3,3 +3,52 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+'''Local Outlier Factor (LOF): The anomaly score of each sample is called Local Outlier Factor. It measures the local deviation of density of a given sample with respect to its neighbors. It is local in that the anomaly score depends on how isolated the object is with respect to the surrounding neighborhood.
+Isolation Forest Algorithm:
+The IsolationForest ‘isolates’ observations by randomly selecting a feature and then randomly selecting a split value between the maximum and minimum values of the selected feature.
+Since recursive partitioning can be represented by a tree structure, the number of splittings required to isolate a sample is equivalent to the path length from the root node to the terminating node.
+This path length, averaged over a forest of such random trees, is a measure of normality and our decision function.
+Random partitioning produces noticeably shorter paths for anomalies. Hence, when a forest of random trees collectively produce shorter path lengths for particular samples, they are highly likely to be anomalies.
+'''
+
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.ensemble import IsolationForest
+from sklearn.neighbors import LocalOutlierFactor
+
+# define random states
+state = 1
+
+# define outlier detection tools to be compared
+classifiers = {
+    "Isolation Forest": IsolationForest(max_samples=len(X), contamination=outlier_fraction, random_state=state),
+    "Local Outlier Factor": LocalOutlierFactor(n_neighbors=20, contamination=outlier_fraction)}
+
+
+# Fit the model
+plt.figure(figsize=(9, 7))
+n_outliers = len(Fraud)
+
+
+for i, (clf_name, clf) in enumerate(classifiers.items()):
+    
+    # fit the data and tag outliers
+    if clf_name == "Local Outlier Factor":
+        y_pred = clf.fit_predict(X)
+        scores_pred = clf.negative_outlier_factor_
+    else:
+        clf.fit(X)
+        scores_pred = clf.decision_function(X)
+        y_pred = clf.predict(X)
+    
+    # Reshape the prediction values to 0 for valid, 1 for fraud. 
+    y_pred[y_pred == 1] = 0
+    y_pred[y_pred == -1] = 1
+    
+    n_errors = (y_pred != Y).sum()
+    
+    # Run classification metrics
+    print('{}: {}'.format(clf_name, n_errors))
+    print(accuracy_score(Y, y_pred))
+    print(classification_report(Y, y_pred))
+    
+    
