05daf69414db4c2de5fd54166c493b061af38299
Rohith Kattamuri <50071276+rkat7@users.noreply.github.com>
2019-07-08 20:46:25 +0530

    Update and rename data.py to data_processing.py

---
+++ data_processing.py
@@ -0,0 +1,53 @@
+# Loading the dataset from the csv file using pandas
+data = pd.read_csv('creditcard.csv')
+
+# Exploring the dataset
+print(data.columns)
+
+# Print the shape of the data
+data = data.sample(frac=0.1, random_state = 1)
+print(data.shape)
+print(data.describe())
+
+# V1 - V28 are the results of a PCA Dimensionality reduction to protect user identities and sensitive features
+
+# Plot histograms of each parameter 
+data.hist(figsize = (20, 20))
+plt.show()
+
+# Determine number of fraud cases in dataset
+
+Fraud = data[data['Class'] == 1]
+Valid = data[data['Class'] == 0]
+
+outlier_fraction = len(Fraud)/float(len(Valid))
+print(outlier_fraction)
+
+print('Fraud Cases: {}'.format(len(data[data['Class'] == 1])))
+print('Valid Transactions: {}'.format(len(data[data['Class'] == 0])))
+
+# Correlation matrix
+corrmat = data.corr()
+fig = plt.figure(figsize = (12, 9))
+
+sns.heatmap(corrmat, vmax = .8, square = True)
+plt.show()
+
+# Get all the columns from the dataFrame
+columns = data.columns.tolist()
+
+# Filter the columns to remove data we do not want
+columns = [c for c in columns if c not in ["Class"]]
+
+# Store the variable we'll be predicting on
+target = "Class"
+
+X = data[columns]
+Y = data[target]
+
+# Print shapes
+print(X.shape)
+print(Y.shape)
+
+
+
